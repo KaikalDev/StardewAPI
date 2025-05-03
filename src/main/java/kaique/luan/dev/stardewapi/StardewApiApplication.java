f38b6a070e4d05787ce319426664746b655c0cf6
Kaique <kaiquesimoes58@gmail.com>
2025-05-03 15:12:22 -0300

    Init do projeto

---
+++ src/main/java/kaique/luan/dev/stardewapi/StardewApiApplication.java
@@ -0,0 +1,13 @@
+package kaique.luan.dev.stardewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StardewApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StardewApiApplication.class, args);
+    }
+
+}
